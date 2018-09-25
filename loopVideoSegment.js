cb0fb05f999c7c8682aa79e4f3d51898d3fa9575
Hemanth Kona <hemanth0690@gmail.com>
2018-09-25 00:07:12 -0400

    Add: loop vide segment

---
+++ loopVideoSegment.js
@@ -0,0 +1,22 @@
+/* loopVideoSegment.js
+ *
+ *  Get first video element on the page and loop from given start and end time
+ *
+ * @param start: integer (seconds)
+ * @param end: integer (seconds)
+ *
+ */
+var startTime = 0; // (seconds) start time offset
+var endTime = 240; // (seconds) end time to restart from startTime
+
+var loopVideoSegment = function (start, end) {
+    var video = document.getElementsByTagName("video")[0];
+
+    video.addEventListener("timeupdate", function() {
+        if (video.currentTime <= start || video.currentTime >= end) {
+            video.currentTime = start;
+        }
+    }, false);
+}
+
+loopVideoSegment(startTime, endTime);
